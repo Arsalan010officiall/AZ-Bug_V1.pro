2e75acfb12a3a9a00e47d41b8f4a52b9f831e9a6
A-R-S-L-A-N_M-D <134489993+Arsalan010officiall@users.noreply.github.com>
2023-07-29 20:27:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('./config')
-const { default: rxhlConnect, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto, getAggregateVotesInPollMessage } = require("@whiskeysockets/baileys")
+const { default: AZBugConnect, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto, getAggregateVotesInPollMessage } = require("@whiskeysockets/baileys")
 const pino = require('pino')
 const { Boom } = require('@hapi/boom')
 const fs = require('fs')
@@ -22,10 +22,10 @@ const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream
 
 async function startrxhl() {
     const { state, saveCreds } = await useMultiFileAuthState('./auth')
-    const rxhl = rxhlConnect({
+    const AZBug = AZBugConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['RXHL-BOT','Safari','1.0.0'],
+        browser: ['AZBug-BOT','Safari','1.0.0'],
         auth: state,
         getMessage: async (key) => {
             if (store) {
@@ -38,34 +38,34 @@ async function startrxhl() {
         }
     })
 
-    store.bind(rxhl.ev)
+    store.bind(AZBug.ev)
     
-    rxhl.ev.on('call', async (caller) => {
+    AZBug.ev.on('call', async (caller) => {
 	    console.log(caller)
     })
 
-    rxhl.ev.on('messages.upsert', async chatUpdate => {
+    AZBug.ev.on('messages.upsert', async chatUpdate => {
 	    // console.log(JSON.stringify(chatUpdate, undefined, 2))
         try {
         msg = chatUpdate.messages[0]
         if (!msg.message) return
         msg.message = (Object.keys(msg.message)[0] === 'ephemeralMessage') ? msg.message.ephemeralMessage.message : msg.message
         // if (msg.key && msg.key.remoteJid === 'status@broadcast') return
-        if (!rxhl.public && !msg.key.fromMe && chatUpdate.type === 'notify') return
+        if (!AZBug.public && !msg.key.fromMe && chatUpdate.type === 'notify') return
         if (msg.key.id.startsWith('BAE5') && msg.key.id.length === 16) return
-        m = smsg(rxhl, msg, store)
-        require("./rxhl")(rxhl, m, chatUpdate, store)
+        m = smsg(AZBug, msg, store)
+        require("./AZBug")(AZBug, m, chatUpdate, store)
         } catch (err) {
             console.log(err)
         }
     })
 
-    rxhl.ev.on('group-participants.update', async (anu) => {
+    AZBug.ev.on('group-participants.update', async (anu) => {
         console.log(anu)
     })
 	
     // Setting
-    rxhl.decodeJid = (jid) => {
+    AZBug.decodeJid = (jid) => {
         if (!jid) return jid
         if (/:\d+@/gi.test(jid)) {
             let decode = jidDecode(jid) || {}
@@ -73,63 +73,63 @@ async function startrxhl() {
         } else return jid
     }
     
-    rxhl.ev.on('contacts.update', update => {
+    AZBug.ev.on('contacts.update', update => {
         for (let contact of update) {
             let id = rxhl.decodeJid(contact.id)
             if (store && store.contacts) store.contacts[id] = { id, name: contact.notify }
         }
     })
 
-    rxhl.getName = (jid, withoutContact  = false) => {
-        id = rxhl.decodeJid(jid)
-        withoutContact = rxhl.withoutContact || withoutContact 
+    AZBug.getName = (jid, withoutContact  = false) => {
+        id = AZBug.decodeJid(jid)
+        withoutContact = AZBug.withoutContact || withoutContact 
         let v
         if (id.endsWith("@g.us")) return new Promise(async (resolve) => {
             v = store.contacts[id] || {}
-            if (!(v.name || v.subject)) v = rxhl.groupMetadata(id) || {}
+            if (!(v.name || v.subject)) v = AZBug.groupMetadata(id) || {}
             resolve(v.name || v.subject || PhoneNumber('+' + id.replace('@s.whatsapp.net', '')).getNumber('international'))
         })
         else v = id === '0@s.whatsapp.net' ? {
             id,
             name: 'WhatsApp'
-        } : id === rxhl.decodeJid(rxhl.user.id) ?
-            rxhl.user :
+        } : id === AZBug.decodeJid(AZBug.user.id) ?
+            AZBug.user :
             (store.contacts[id] || {})
             return (withoutContact ? '' : v.name) || v.subject || v.verifiedName || PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international')
     }
     
-    rxhl.sendContact = async (jid, kon, quoted = '', opts = {}) => {
+    AZBug.sendContact = async (jid, kon, quoted = '', opts = {}) => {
 	let list = []
 	for (let i of kon) {
 	    list.push({
-	    	displayName: await rxhl.getName(i),
-	    	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await rxhl.getName(i)}\nFN:${await rxhl.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+	    	displayName: await AZBug.getName(i),
+	    	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await AZBug.getName(i)}\nFN:${await AZBug.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
 	    })
 	}
-	rxhl.sendMessage(jid, { contacts: { displayName: `${list.length} Kontak`, contacts: list }, ...opts }, { quoted })
+	AZBug.sendMessage(jid, { contacts: { displayName: `${list.length} Kontak`, contacts: list }, ...opts }, { quoted })
     }
     
-    rxhl.public = true
-    rxhl.serializeM = (m) => smsg(rxhl, m, store)
+    AZBug.public = true
+    AZBug.serializeM = (m) => smsg(AZBug, m, store)
 
-    rxhl.ev.on('connection.update', async (update) => {
+    AZBug.ev.on('connection.update', async (update) => {
         const { connection, lastDisconnect } = update	    
         if (connection === 'close') {
         let reason = new Boom(lastDisconnect?.error)?.output.statusCode
             if (reason === DisconnectReason.badSession) { console.log(`Bad Session File, Please Delete Session and Scan Again`); rxhl.logout(); }
-            else if (reason === DisconnectReason.connectionClosed) { console.log("Connection closed, reconnecting...."); startrxhl(); }
-            else if (reason === DisconnectReason.connectionLost) { console.log("Connection Lost from Server, reconnecting..."); startrxhl(); }
-            else if (reason === DisconnectReason.connectionReplaced) { console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); rxhl.logout(); }
-            else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out, Please Scan Again And Run.`); rxhl.logout(); }
-            else if (reason === DisconnectReason.restartRequired) { console.log("Restart Required, Restarting..."); startrxhl(); }
-            else if (reason === DisconnectReason.timedOut) { console.log("Connection TimedOut, Reconnecting..."); startrxhl(); }
-            else if (reason === DisconnectReason.Multidevicemismatch) { console.log("Multi device mismatch, please scan again"); rxhl.logout(); }
-            else rxhl.end(`Unknown DisconnectReason: ${reason}|${connection}`)
+            else if (reason === DisconnectReason.connectionClosed) { console.log("Connection closed, reconnecting...."); startAZBug(); }
+            else if (reason === DisconnectReason.connectionLost) { console.log("Connection Lost from Server, reconnecting..."); startAZBug(); }
+            else if (reason === DisconnectReason.connectionReplaced) { console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); AZBug.logout(); }
+            else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out, Please Scan Again And Run.`); AZBug.logout(); }
+            else if (reason === DisconnectReason.restartRequired) { console.log("Restart Required, Restarting..."); startAZBug(); }
+            else if (reason === DisconnectReason.timedOut) { console.log("Connection TimedOut, Reconnecting..."); startAZBug(); }
+            else if (reason === DisconnectReason.Multidevicemismatch) { console.log("Multi device mismatch, please scan again"); AZBug.logout(); }
+            else AZBug.end(`Unknown DisconnectReason: ${reason}|${connection}`)
         }
         console.log('Connected...', update)
     })
 
-    rxhl.ev.on('creds.update', saveCreds)
+    AZBug.ev.on('creds.update', saveCreds)
 
     /**
      * 
@@ -139,7 +139,7 @@ async function startrxhl() {
      * @param {*} options 
      * @returns 
      */
-    rxhl.copyNForward = async (jid, message, forceForward = false, options = {}) => {
+    AZBu.copyNForward = async (jid, message, forceForward = false, options = {}) => {
         let vtype
 		if (options.readViewOnce) {
 			message.message = message.message && message.message.ephemeralMessage && message.message.ephemeralMessage.message ? message.message.ephemeralMessage.message : (message.message || undefined)
@@ -170,11 +170,11 @@ async function startrxhl() {
                 }
             } : {})
         } : {})
-        await rxhl.relayMessage(jid, waMessage.message, { messageId:  waMessage.key.id })
+        await AZBug.relayMessage(jid, waMessage.message, { messageId:  waMessage.key.id })
         return waMessage
     }
 
-    rxhl.cMod = (jid, copy, text = '', sender = rxhl.user.id, options = {}) => {
+    AZBug.cMod = (jid, copy, text = '', sender = AZBug.user.id, options = {}) => {
         //let copy = message.toJSON()
 		let mtype = Object.keys(copy.message)[0]
 		let isEphemeral = mtype === 'ephemeralMessage'
@@ -195,7 +195,7 @@ async function startrxhl() {
 		if (copy.key.remoteJid.includes('@s.whatsapp.net')) sender = sender || copy.key.remoteJid
 		else if (copy.key.remoteJid.includes('@broadcast')) sender = sender || copy.key.remoteJid
 		copy.key.remoteJid = jid
-		copy.key.fromMe = sender === rxhl.user.id
+		copy.key.fromMe = sender === AZBug.user.id
 
         return proto.WebMessageInfo.fromObject(copy)
     }
@@ -209,7 +209,7 @@ async function startrxhl() {
 
  
      
-    rxhl.getFile = async (PATH, save) => {
+    AZBug.getFile = async (PATH, save) => {
         let res
         let data = Buffer.isBuffer(PATH) ? PATH : /^data:.*?\/.*?;base64,/i.test(PATH) ? Buffer.from(PATH.split`,`[1], 'base64') : /^https?:\/\//.test(PATH) ? await (res = await getBuffer(PATH)) : fs.existsSync(PATH) ? (filename = PATH, fs.readFileSync(PATH)) : typeof PATH === 'string' ? PATH : Buffer.alloc(0)
         //if (!Buffer.isBuffer(data)) throw new TypeError('Result is not a buffer')
@@ -230,10 +230,10 @@ async function startrxhl() {
     }
     
 
-    return rxhl
+    return AZBug
 }
 
-startrxhl()
+startAZBug()
 
 
 let file = require.resolve(__filename)
